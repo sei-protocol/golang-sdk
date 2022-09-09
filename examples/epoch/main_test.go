b818919e3173b3d695aa4b9cf6f353f2a362ef6e
Brandon Weng <wengbrandon@gmail.com>
2022-09-09 02:17:08 -0700

    typo

--- examples/epoch/main_test.go
+++ examples/epoch/main_test.go
@@ -13,6 +13,6 @@ func TestGetEpoch(t *testing.T) {
 	seiClient := seiSdk.NewClientWithDefaultConfig(secp256k1.GenPrivKey())
 
 	currentEpoch := seiClient.GetCurrentEpoch()
-	println("Current Epoch: {}", currentEpoch)
+	println("Current Epoch:", currentEpoch)
 	assert.GreaterOrEqual(t, currentEpoch, uint64(0), "Epoch should be >= 0")
 }
